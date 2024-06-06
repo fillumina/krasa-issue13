137805ad91c2197a3171bdae9cba795da861de53
Francesco Illuminati <fillumina@gmail.com>
2024-06-06 23:15:14 +0200

    working project with cxf-codegen-plugin v 3.2.4

---
+++ src/main/java/com/fillumina/krasa/issue13/KrasaIssue13.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.fillumina.krasa.issue13;
+
+/**
+ *
+ * @author Francesco Illuminati <fillumina@gmail.com>
+ */
+public class KrasaIssue13 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
